2ecba95704bc178979b4924039d715a85ef9c57d
KinFol <>
2019-11-24 20:53:50 +0000

    add before each block

--- src/test/unit/todo-model.test.js
+++ src/test/unit/todo-model.test.js
@@ -6,6 +6,17 @@ const newTodo = require('../mock-data/new-todo.json')
 //mongoose model has different methods. Here we use the create method.
 TodoModel.create = jest.fn();
 
+// as the request, response and next will be used in multiple tests
+// we make use of beforeEach() function to initialise and avoid duplication
+
+let req, res, next;
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createRequest();
+    next = null;
+    req.body = newTodo;
+});
+
 describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
     expect(typeof TodoController.toDo).toBe("function");
@@ -16,11 +27,6 @@ describe("TodoController.createTodo", () => {
   to create a model 
   Note: we don't want to test the implementation, as that is part of the integration testing*/
   it("should call TodoModel.create", () => {
-    let req, res, next;
-    req = httpMocks.createRequest();
-    res = httpMocks.createRequest();
-    next = null;
-    req.body = newTodo;
     //instantiating the todo-controller class
     const p1 = new TodoController.toDo()
     //calling the method from the class.
