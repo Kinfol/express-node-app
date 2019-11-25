f889ace554427fc94493b3abb2d06e6ee2e48ff7
KinFol <>
2019-11-25 07:55:54 +0000

    test the respose. add method to provide with 201 response

--- src/constrollers/todo-controller.js
+++ src/constrollers/todo-controller.js
@@ -4,9 +4,15 @@ const TodoModel = require ('../model/todo-model')
 class ToDo {
     createTodo(req, res, next){
         return TodoModel.create(req.body);
+        
+    }
+
+    createResponse(req, res, next){
+        res.status(201);
     }
 
 }
+
 
 module.exports = {
     "toDo":ToDo
