4a8bfbacfc11df011ab0a2562743ad73b953187a
KinFol <>
2019-11-25 21:34:39 +0000

    pass test

--- src/constrollers/todo-controller.js
+++ src/constrollers/todo-controller.js
@@ -4,13 +4,12 @@ const TodoModel = require ('../model/todo-model')
 class ToDo {
     createTodo(req, res, next){
         return TodoModel.create(req.body);
-        
     }
 
     createResponse(req, res, next){
-        res.status(201).send();
+        const createModel  = TodoModel.create(req.body);
+        res.status(201).json(createModel);
     }
-
 }
 
 
